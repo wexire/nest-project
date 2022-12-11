26fadf779e6c836c192aeb1f7b2ed3e888f62b1d
wexire <peter.pol.code@gmail.com>
2022-12-11 13:15:39 +0200

    schemas & auth dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    PrismaModule,
+    ConfigModule.forRoot(),
+  ],
   controllers: [],
   providers: [],
 })
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Post } from '@nestjs/common';
+import { Request } from 'express';
+import { AuthDto } from './dto';
 
 @Controller({})
 export class AuthContoller {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  login() {
+  login(@Body() dto: AuthDto) {
     return this.authService.login();
   }
 
