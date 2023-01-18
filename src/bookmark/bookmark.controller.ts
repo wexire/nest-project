6c04c9489fd35743c5ab9dd6172d6def35f1eb8d
wexire <peter.pol.code@gmail.com>
2023-01-18 20:51:48 +0200

    bookmark service

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -20,30 +20,40 @@ export class BookmarkController {
   constructor(private bookmarkService: BookmarkService) {}
 
   @Get()
-  getBookmarks(@GetUser('id') userId: number) {}
+  getBookmarks(@GetUser('id') userId: number) {
+    return this.bookmarkService.getBookmarks(userId);
+  }
 
   @Get(':id')
   getBookmarkById(
     @GetUser('id') userId: number,
     @Param('id', ParseIntPipe) bookmarkId: number,
-  ) {}
+  ) {
+    return this.getBookmarkById(userId, bookmarkId);
+  }
 
   @Post()
   createBookmark(
     @GetUser('id') userId: number,
     @Body() dto: CreateBookmarkDto,
-  ) {}
+  ) {
+    return this.createBookmark(userId, dto);
+  }
 
   @Patch(':id')
   editBookmarkById(
     @GetUser('id') userId: number,
     @Param('id', ParseIntPipe) bookmarkId: number,
     @Body() dto: EditBookmarkDto,
-  ) {}
+  ) {
+    return this.editBookmarkById(userId, bookmarkId, dto);
+  }
 
   @Delete(':id')
   deleteBookmarkById(
     @GetUser('id') userId: number,
     @Param('id', ParseIntPipe) bookmarkId: number,
-  ) {}
+  ) {
+    return this.deleteBookmarkById(userId, bookmarkId);
+  }
 }
